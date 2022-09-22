2e33269b723044b0d40bfe1c11cb62f07a9d6d9c
miguel alejandro fernandez arteaga <53372986+bash20cu@users.noreply.github.com>
2022-09-21 21:18:50 -0600

    Uso del java.util.Scanner
    Uso del java.util.Scanner, Realizacion del Ejercicio 33, ejecución de un presupuesto.

--- Ejercicios/pytsemana1/src/pytsemana1/Pytsemana1.java
+++ Ejercicios/pytsemana1/src/pytsemana1/Pytsemana1.java
@@ -4,6 +4,8 @@
  */
 package pytsemana1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author migue
@@ -14,7 +16,32 @@ public class Pytsemana1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       byte edad = 18;
+       int cantidad = 150;
+       short existncias = 3500;
+       long valor = 3500000;
+       double importe = 155.55;
+       double precio = 155.55;
+       boolean encontrado = true;
+       String nombre = "Juan";
+       
+       final double DOLARVENTA = 632.35;
+       
+       edad = 100;
+       
+       importe = cantidad * precio;
+     
+       
+        System.out.println("");
+       
+       
+        
+       
+       
+               
+               
+               
+       
     }
     
 }
