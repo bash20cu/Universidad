507bc994275265145e1fff1a2d671cf6b5dcfe00
miguel alejandro fernandez arteaga <53372986+bash20cu@users.noreply.github.com>
2022-11-15 12:55:12 -0600

    Arbol Binario
    Creacion de arbol binario

---
+++ ArbolBusqueda/src/arbolBB/Integrantes.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arbolBB;
+
+/**
+ *
+ * @author Toloza XD
+ */
+public class Integrantes {
+    
+    /* Reyna Marinely Cerritos Cornejo CC16124
+    Enrique Josué Cortez Hernández     CH14008
+    Reynaldo Eliseo Diaz Molina        DM16015
+    Roberto Alexander Toloza Mendoza   TM16010
+    */
+    
+}
---
+++ binaryTree/src/binarytree/clsNode.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package binarytree;
+
+import org.w3c.dom.Node;
+
+/**
+ *
+ * @author migue
+ */
+public class clsNode {
+    int value;
+    Node left;
+    Node right;
+
+    clsNode (int value) {
+        this.value = value;
+        right = null;
+        left = null;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+    
+
+}
