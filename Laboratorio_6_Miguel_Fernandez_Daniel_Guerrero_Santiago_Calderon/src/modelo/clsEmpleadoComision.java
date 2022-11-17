2f46e26e22d551bff7a09934da6b5497922e141a
miguel alejandro fernandez arteaga <53372986+bash20cu@users.noreply.github.com>
2022-11-16 21:52:45 -0600

    laboratorio 6
    creacion de la clase, empleado por comision

--- Laboratorio_6_Miguel_Fernandez_Daniel_Guerrero_Santiago_Calderon/src/modelo/clsEmpleadoComision.java
+++ Laboratorio_6_Miguel_Fernandez_Daniel_Guerrero_Santiago_Calderon/src/modelo/clsEmpleadoComision.java
@@ -9,15 +9,68 @@ package modelo;
  * @author migue
  */
 public class clsEmpleadoComision extends clsEmpleado implements iPorcentajeVentas {
+    private double comision;
+    private int totalVentas;
+    private double salarioSemanal;
 
-    @Override
-    public double importeNomina() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public clsEmpleadoComision() {
+        super();
+    }
+    
+    
+    public clsEmpleadoComision(double comision, int totalVentas) {
+        this.comision = comision;
+        this.totalVentas = totalVentas;
     }
 
+    public clsEmpleadoComision(double comision, int totalVentas, String empleadoID, String nombre, String apellido1, String apellido2, String telefono) {
+        super(empleadoID, nombre, apellido1, apellido2, telefono);
+        this.comision = comision;
+        this.totalVentas = totalVentas;
+    }
+
+    public double getComision() {
+        return comision;
+    }
+
+    public void setComision(double comision) {
+        this.comision = comision;
+    }
+
+    public int getTotalVentas() {
+        return totalVentas;
+    }
+
+    public void setTotalVentas(int totalVentas) {
+        this.totalVentas = totalVentas;
+    }
+
+    public double getSalarioSemanal() {
+        return salarioSemanal;
+    }
+
+    public void setSalarioSemanal(double salarioSemanal) {
+        this.salarioSemanal = salarioSemanal;
+    }
+    
+        
+    
+   @Override
+    public double importeNomina() {
+        if (getComision() > 0) {
+            setSalarioSemanal(getSalarioSemanal() + porcentajeComision());                    
+            System.out.println("Salario semanal + Comision : "+ salarioSemanal);            
+        }else{
+            System.out.println("Importe en nomina : No tuvo ventas");            
+        }
+        System.out.println("Total a pagar: " + getSalarioSemanal());
+        return (getSalarioSemanal()); //Salario Semanal
+    }            
+    //Metodo para determinar la comision a pagar
     @Override
-    public double porcentajeComision() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public double porcentajeComision() {        
+        System.out.println("Total de comision: " + (getComision() * getTotalVentas()));
+        return (getComision() * getTotalVentas());
     }
     
 }
--- Laboratorio_6_Miguel_Fernandez_Daniel_Guerrero_Santiago_Calderon/src/vista/laboratorio.java
+++ Laboratorio_6_Miguel_Fernandez_Daniel_Guerrero_Santiago_Calderon/src/vista/laboratorio.java
@@ -7,6 +7,7 @@ package vista;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import modelo.clsEmpleadoComision;
 import modelo.clsEmpleadoSemanal;
 import modelo.clsEmpleadoPorHoras;
 
@@ -23,35 +24,45 @@ public class laboratorio {
         short opc = 1; //Opcion para cerrar el ciclo.
         List<clsEmpleadoSemanal> nominaSemanal = new ArrayList<>();
         List<clsEmpleadoPorHoras> nominaPorHoras = new ArrayList<>();
+        List<clsEmpleadoComision> nominaPorComision = new ArrayList<>();
         Scanner in =  new Scanner(System.in);
-        while (opc != 4) {
-            System.out.print("Menu: Digite (1) Introducir empleado semanal, (2) Introducir empleado por horas,(3) Imprimir Nomina, (4) Salir : ");
+        while (opc != 5) {
+            System.out.println("Menu:  Digite");
+            System.out.println("(1) Introducir empleado semanal");
+            System.out.println("(2) Introducir empleado por horas");
+            System.out.println("(3) Introducir empleado por comision");
+            System.out.println("(4) Imprimir Nomina");
+            System.out.println("(5) Salir");
+            
             opc = in.nextShort();            
             switch(opc){
                 case 1:
-                    empleadoSemanal(nominaSemanal, in);
-                    //introducirDatos(nominaSemanal,nominaPorHoras, in);
+                    empleadoSemanal(nominaSemanal, in);                    
                     break;
                 case 2:
                     empleadoXhoras(nominaPorHoras, in);
-                    //introducirDatos(nominaSemanal,nominaPorHoras, in);
+                    break;
+                case 3:
+                    empleadoXComision(nominaPorComision, in);
+                    break;
+                case 4:                    
+                    imprimirNomina(nominaSemanal,nominaPorHoras,nominaPorComision);
                     break; 
-                case 3:                    
-                    imprimirNomina(nominaSemanal,nominaPorHoras);
-                    break; 
-                case 4:
+                case 5:
                     in.close();
                     System.out.println("----- Saliendo del programa -----"); 
                     break;
                 default:
                     System.out.println("Opción Invalida");
                     break;
-            }
+            }            
         }
-
+        in.close();
     }
 
-    private static void imprimirNomina(List<clsEmpleadoSemanal> nominaSemanal, List<clsEmpleadoPorHoras> nominaPorHoras) {
+    private static void imprimirNomina(List<clsEmpleadoSemanal> nominaSemanal, 
+            List<clsEmpleadoPorHoras> nominaPorHoras, 
+            List<clsEmpleadoComision> nominaPorComision) {
        System.out.println("Nomina Semanal Mostrando datos");        
         nominaSemanal.forEach(empleado -> {
             System.out.println("");
@@ -60,6 +71,11 @@ public class laboratorio {
         });        
         
         nominaPorHoras.forEach(empleado -> {
+            System.out.println(empleado.nombreCompleto());
+            System.out.println(empleado.importeNomina());
+        });
+        
+        nominaPorComision.forEach(empleado -> {
             System.out.println(empleado.nombreCompleto());
             System.out.println(empleado.importeNomina());
         });
@@ -109,7 +125,7 @@ public class laboratorio {
     }
 
     private static void empleadoXhoras(List<clsEmpleadoPorHoras> nominaPorHoras, Scanner in) {
-        System.out.println("-- Ingresando datos de empleados por horas: --  ");        
+        System.out.println("-- Ingresando datos de empleados por horas: --  ");
         System.out.println("Digite: (1) - Empleado sin comision, (2) - Empleado con comision");
         clsEmpleadoPorHoras empleado = new clsEmpleadoPorHoras();
         int opcComision = in.nextInt();
@@ -118,11 +134,11 @@ public class laboratorio {
                 System.out.print("Codigo del empleado: ");
                 empleado.setEmpleadoID(in.next());
                 System.out.print("Nombre del empleado: ");
-                empleado.setNombre( in.next());
+                empleado.setNombre(in.next());
                 System.out.print("Primer apellido del empleado: ");
-                empleado.setApellido1(in.next());            
+                empleado.setApellido1(in.next());
                 System.out.print("Segundo apellido del empleado: ");
-                empleado.setApellido2(in.next());            
+                empleado.setApellido2(in.next());
                 System.out.print("Telefono del empleado: ");
                 empleado.setTelefono(in.next());
                 System.out.print("Cantidad de horas trabajadas durante la semana: ");
@@ -135,17 +151,17 @@ public class laboratorio {
                 System.out.print("Codigo del empleado: ");
                 empleado.setEmpleadoID(in.next());
                 System.out.print("Nombre del empleado: ");
-                empleado.setNombre( in.next());
+                empleado.setNombre(in.next());
                 System.out.print("Primer apellido del empleado: ");
-                empleado.setApellido1(in.next());            
+                empleado.setApellido1(in.next());
                 System.out.print("Segundo apellido del empleado: ");
-                empleado.setApellido2(in.next());            
+                empleado.setApellido2(in.next());
                 System.out.print("Telefono del empleado: ");
                 empleado.setTelefono(in.next());
                 System.out.print("Cantidad de horas trabajadas durante la semana: ");
                 empleado.setHorasTrabajadas(in.nextInt());
                 System.out.print("Tasa de pago por horas: ");
-                empleado.setTasaxhoras(in.nextDouble());               
+                empleado.setTasaxhoras(in.nextDouble());
                 System.out.print("Total de ventas: ");
                 empleado.setTotalVentas(in.nextInt());
                 System.out.print("Tasa comision: ");
@@ -155,6 +171,25 @@ public class laboratorio {
             default:
                 System.out.println("Opcion no valida");
         }
+    }
+
+    private static void empleadoXComision(List<clsEmpleadoComision> nominaPorComision, Scanner in) {
+        clsEmpleadoComision empleado = new clsEmpleadoComision();
+        System.out.print("Codigo del empleado: ");
+        empleado.setEmpleadoID(in.next());
+        System.out.print("Nombre del empleado: ");
+        empleado.setNombre(in.next());
+        System.out.print("Primer apellido del empleado: ");
+        empleado.setApellido1(in.next());
+        System.out.print("Segundo apellido del empleado: ");
+        empleado.setApellido2(in.next());
+        System.out.print("Telefono del empleado: ");
+        empleado.setTelefono(in.next());
+        System.out.print("Tasa comision: ");
+        empleado.setComision(in.nextDouble());
+        System.out.print("Total de ventas: ");
+        empleado.setTotalVentas(in.nextInt());
+        nominaPorComision.add(empleado);        
     }
   
 
