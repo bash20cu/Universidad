5c39c89e98bd1b284b60d302307677d690a59672
Miguel Alejandro Fernandez Arteaga <53372986+bash20cu@users.noreply.github.com>
2023-09-13 18:17:18 -0600

    CSharp window form
    Chsarp window form

---
+++ Powershell_Scripts/Service_Control_Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using Service_Control_Console;
+using System;
+using System.ServiceProcess;
+
+class Program
+{
+    static void Main()
+    {
+        Service_Control services = new Service_Control();
+
+        services.hello();
+
+
+    }
+}
---
+++ Powershell_Scripts/Services_Control/Services.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Security.Principal;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Media;
+
+namespace Services_Control
+{   
+    class Services
+    {
+        private MainWindow mainWindow;
+        public Services(MainWindow main) 
+        {
+            mainWindow = main;
+            TestAdminRights();
+
+            ListarServicios();
+        }
+        
+        private void TestAdminRights() 
+        {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+
+            // Comprueba si el usuario actual tiene derechos administrativos
+            bool isAdmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
+            mainWindow.Label1.Content = isAdmin;
+
+        }
+
+        public void ListarServicios()
+        {
+            ServiceController[] services = ServiceController.GetServices();
+            mainWindow.Texto.Text = "";
+
+            // Crear un StringBuilder para almacenar los resultados
+            StringBuilder resultado = new StringBuilder();
+
+            bool isMySQLSelected = mainWindow.mysql.IsChecked ?? false;
+            bool isMSSQLSelected = mainWindow.mssql.IsChecked ?? false;
+
+            foreach (ServiceController service in services)
+            {
+                if ((isMySQLSelected  && service.DisplayName.Contains("MySQL")) ||
+                    (isMSSQLSelected  && service.DisplayName.Contains("SQL Server")))
+                {
+                    resultado.AppendLine("Nombre del servicio: " + service.ServiceName);
+                    resultado.AppendLine("Estado del servicio: " + service.Status);
+                    resultado.AppendLine("--------------------------------------");
+                }
+
+                // Agregar la información del servicio al TextBox
+                mainWindow.Texto.Text = resultado.ToString();
+            }
+        }
+
+    }
+}
